93f566d50718a8d0d0a5713bd780012764a405d8
l01c d3v <l01cd3v@gmail.com>
2015-08-16 23:16:06 -0700

    Do not copy past code and add some verbose errors upon test failure

--- tests/test-listall.py
+++ tests/test-listall.py
@@ -7,6 +7,14 @@ import subprocess
 class TestListAllClass:
 
     #
+    # Bleh
+    #
+    listall_configs = [
+        ['sample-001', 'iam-users-without-mfa-in-select-groups.json', ['MisconfiguredUser-NoMFA', 'l01cd3v-test']],
+        ['sample-001', 'iam-users-without-mfa-not-in-select-groups.json', ['l01cd3v-test']]
+    ]
+
+    #
     # Make sure that ListAll does not crash with --help
     #
     def test_listall_help(self):
@@ -16,25 +24,19 @@ class TestListAllClass:
         assert process.returncode == 0
 
     #
-    # Test config: iam-users-without-mfa-in-select-groups.json
+    # Test all configs
     #
-    def test_listall_config_1(self):
-        command = './ListAll.py --env sample-001 --config listall-configs/iam-users-without-mfa-in-select-groups.json'
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        process.wait()
-        lines = []
-        for line in process.stdout:
-            lines.append(line.strip())
-        assert sorted(lines) == sorted(['MisconfiguredUser-NoMFA', 'l01cd3v-test'])
-
-    #
-    # Test config: iam-users-without-mfa-not-in-select-groups.json
-    #
-    def test_listall_config_2(self):
-        command = './ListAll.py --env sample-001 --config listall-configs/iam-users-without-mfa-not-in-select-groups.json'
-        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        process.wait()
-        lines = []
-        for line in process.stdout:
-            lines.append(line.strip())
-        assert sorted(lines) == sorted(['l01cd3v-test'])
+    def test_listall_configs(self):
+        all_passed = True
+        for config in self.listall_configs:
+            command = './ListAll.py --env %s --config listall-configs/%s' % (config[0], config[1])
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process.wait()
+            lines = []
+            for line in process.stdout:
+                lines.append(line.strip())
+            print lines
+            if sorted(lines) != sorted(config[2]):
+                print('Error when testing %s :: %s does not match %s' % (config[1], lines, config[2]))
+                all_passed = False
+        assert all_passed
